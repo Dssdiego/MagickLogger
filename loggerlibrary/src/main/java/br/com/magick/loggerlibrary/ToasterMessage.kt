53853ae1c74ec4f5d03bebb036163ce891fc5938
Diego S. Seabra <dss-diego@hotmail.com>
2021-03-18 21:48:33 -0300

    Initial Project

---
+++ loggerlibrary/src/main/java/br/com/magick/loggerlibrary/ToasterMessage.kt
@@ -0,0 +1,12 @@
+package br.com.magick.loggerlibrary
+
+import android.content.Context
+import android.widget.Toast
+
+class ToasterMessage {
+    companion object {
+        fun s(context: Context, message: String) {
+            Toast.makeText(context, message, Toast.LENGTH_SHORT).show()
+        }
+    }
+}
